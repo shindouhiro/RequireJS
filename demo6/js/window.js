9c93e6b1b386723b0b51d36adf6e4184c2953237
shindouhiro <you@example.com>
2015-02-03 11:47:38 +0800

    six

---
+++ demo6/js/window.js
@@ -0,0 +1,45 @@
+define(['jquery'],function($){
+	
+	function Window(){
+		this.cfg = {
+			width: 500,
+			height: 500,
+			content: '',
+			handler: null,
+			title: '提示'
+		}
+	}
+	
+	Window.prototype = {
+		alert:function(cfg){
+			var CFG = $.extend(this.cfg,cfg);
+			var boundingBox = $(
+				'<div class="window_boundingBox">'+
+					'<div class="window_header">'+CFG.title+'</div>'+
+					'<div class="window_body">'+CFG.content+'</div>'+
+					'<div class="window_footer"><input type="button" value="确定"></div>'+
+				'</div>'
+				);
+				btn = boundingBox.find(".window_footer input");
+			    boundingBox.appendTo('body');
+				btn.click(function(){
+					CFG.handler && CFG.handler();
+					boundingBox.remove();
+				})
+				
+				boundingBox.css({
+					width: CFG.width+"px",
+					height: CFG.height+"px",
+					left: (CFG.x || (Window.innerWidth-CFG.width)/2)+"px",
+					top: (CFG.y || (Window.innerHeight-CFG.height/2))+"px"
+				})
+		},
+		confirm:function(){
+		},
+		prompt:function(){}
+	}
+	
+	return{
+		Window : Window
+	}
+})
